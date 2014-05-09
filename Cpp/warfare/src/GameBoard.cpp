0271794c31b02fb358a7cae6a626bd00300bc3d9
Vitor Rozsa <vitor.rozsa@hotmail.com>
2014-05-09 19:44:49 -0300

    [warfare] Playing with the board class.

--- Cpp/warfare/src/GameBoard.cpp
+++ Cpp/warfare/src/GameBoard.cpp
@@ -6,10 +6,15 @@
  */
 
 #include <iostream>
+#include <Windows.h>
 #include "assert.h"
 #include "GameBoard.h"
 
 #include "VisualElement.h"
+
+#define RED 255
+#define GREEN 0
+#define BLUE 255
 
 GameBoard::GameBoard(Video& video, unsigned int width, unsigned int height):
 m_Video(video),
@@ -26,18 +31,37 @@ GameBoard::~GameBoard() {
 
 void GameBoard::draw()
 {
-	VisualElement foo;
+	VisualElement foo, fa(50,50);
 
-	SDL_Surface *img = SDL_LoadBMP("sprites/warrior.bmp");
+	SDL_Surface *img = SDL_LoadBMP("sprites/grass.bmp");
+	SDL_SetColorKey(img , SDL_SRCCOLORKEY, SDL_MapRGB(img->format, RED, GREEN, BLUE));
 	//assert(img);
 
 	foo.set_viewpoint(img, 0);
+	fa.set_viewpoint(img, 0);
 
 	m_Video.add_visualElement(&foo);
+	m_Video.add_visualElement(&fa);
+
+	unsigned char width = 5;
+	unsigned char height = 5;
+
+	unsigned int hwidth = 32;
+	unsigned int hheight = 16;
+
+
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			SDL_Surface *img = SDL_LoadBMP("sprites/warrior.bmp");
+			cout << "x: " << int(hwidth*x) << " y: " << int(hheight*y) << endl;
+			VisualElement fa (int(hwidth*x), int(hheight*y));
+			fa.set_viewpoint(img, 0);
+			m_Video.add_visualElement(&fa);
+		}
+	}
 	m_Video.start();
 
-	int a;
-	std::cin >> a;
+	Sleep(3000);
 
-	SDL_FreeSurface(img);
+	//SDL_FreeSurface(img);
 }
